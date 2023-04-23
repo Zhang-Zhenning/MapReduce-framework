c3b8ca5198818335feddf907474a60f09bfca668
Zhenning Zhang <zz45@illinois.edu>
2023-04-22 22:43:12 -0500

    final version

---
+++ map_reduce_func.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func WcMapF(filename string, contents string) []KeyValue {
+	wordsKv := make(map[string]int)
+
+	words := strings.FieldsFunc(contents, func(r rune) bool {
+		return !unicode.IsLetter(r)
+	})
+	for _, word := range words {
+		if _, ok := wordsKv[word]; ok {
+			wordsKv[word]++
+		} else {
+			wordsKv[word] = 1
+		}
+	}
+
+	var rst []KeyValue
+	for key, value := range wordsKv {
+		kv := KeyValue{
+			key,
+			strconv.Itoa(value),
+		}
+		rst = append(rst, kv)
+	}
+	return rst
+}
+
+func WcReduceF(key string, values []string) string {
+	cnt := 0
+	for _, value := range values {
+		num, err := strconv.Atoi(value)
+		if err != nil {
+			break
+		}
+		cnt += num
+	}
+	return strconv.Itoa(cnt)
+}
--- test.go
+++ test.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"runtime"
 	"strconv"
-	"strings"
-	"unicode"
 )
 
 type TEST struct{}
@@ -14,44 +12,6 @@ type TEST struct{}
 func (t *TEST) HelloWorld(args *string, reply *string) error {
 	*reply = "Hello, " + *args
 	return nil
-}
-
-// test map and reduce functions for word count
-func WcMapF(filename string, contents string) []KeyValue {
-	wordsKv := make(map[string]int)
-
-	words := strings.FieldsFunc(contents, func(r rune) bool {
-		return !unicode.IsLetter(r)
-	})
-	for _, word := range words {
-		if _, ok := wordsKv[word]; ok {
-			wordsKv[word]++
-		} else {
-			wordsKv[word] = 1
-		}
-	}
-
-	var rst []KeyValue
-	for key, value := range wordsKv {
-		kv := KeyValue{
-			key,
-			strconv.Itoa(value),
-		}
-		rst = append(rst, kv)
-	}
-	return rst
-}
-
-func WcReduceF(key string, values []string) string {
-	cnt := 0
-	for _, value := range values {
-		num, err := strconv.Atoi(value)
-		if err != nil {
-			break
-		}
-		cnt += num
-	}
-	return strconv.Itoa(cnt)
 }
 
 func main() {
@@ -85,5 +45,6 @@ func main() {
 	// wait for the Master to finish
 	// all workers should exit before the Master does
 	<-ret_c
+	os.RemoveAll(s)
 
 }
